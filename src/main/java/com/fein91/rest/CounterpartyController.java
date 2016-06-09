fa6bbb7e3128f5ede6336a692cc04ff542bf664f
Artem <makogon.artem@gmail.com>
2016-06-10 00:56:53 +0200

    added unit tests for rest controllers

--- src/main/java/com/fein91/rest/CounterpartyController.java
+++ src/main/java/com/fein91/rest/CounterpartyController.java
@@ -2,33 +2,25 @@ package com.fein91.rest;
 
 import com.fein91.dao.InvoiceRepository;
 import com.fein91.model.Invoice;
-import com.fein91.model.ProposalInfo;
-import com.fein91.service.CounterPartyService;
-import com.fein91.service.CounterPartyServiceImpl;
-import com.fein91.service.OrderRequestService;
-import com.fein91.service.OrderRequestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @RestController
 @RequestMapping("/counterparties")
 public class CounterpartyController {
 
+    private final InvoiceRepository invoiceRepository;
+
     @Autowired
-    InvoiceRepository invoiceRepository;
-    @Autowired
-    @Qualifier("OrderRequestServiceImpl")
-    OrderRequestService orderRequestServiceImpl;
-    @Autowired
-    @Qualifier("CounterPartyServiceImpl")
-    CounterPartyService counterPartyServiceImpl;
+    public CounterpartyController(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
+
 
     @RequestMapping(method = RequestMethod.GET, value = "/{sourceId}/invoicesBySource")
     public List<Invoice> getBySourceId(@PathVariable Long sourceId) {
--- src/main/java/com/fein91/rest/OrderRequestController.java
+++ src/main/java/com/fein91/rest/OrderRequestController.java
@@ -1,23 +1,24 @@
 package com.fein91.rest;
 
-import com.fein91.model.OrderResult;
 import com.fein91.model.OrderRequest;
+import com.fein91.model.OrderResult;
 import com.fein91.service.OrderRequestService;
-import com.fein91.service.OrderRequestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @RestController
 @RequestMapping("/orderRequests")
 public class OrderRequestController {
 
+    private final OrderRequestService orderRequestServiceImpl;
+
     @Autowired
-    @Qualifier("OrderRequestServiceImpl")
-    OrderRequestService orderRequestServiceImpl;
+    public OrderRequestController(@Qualifier("OrderRequestServiceImpl") OrderRequestService orderRequestServiceImpl) {
+        this.orderRequestServiceImpl = orderRequestServiceImpl;
+    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/process")
     public OrderResult process(@RequestBody OrderRequest orderRequest) {
