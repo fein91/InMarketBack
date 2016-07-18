dbbb26c6ca718b588075f2ea6b70616b929e2fda
fein91 <alexot91@gmail.com>
2016-07-18 23:53:36 +0300

    template implemented

--- src/main/resources/static/resources/app.js
+++ src/main/resources/static/resources/app.js
@@ -105,3 +105,13 @@ angular.module('inmarket', [
             })
         }
     }]);
+
+    //here comes some static js
+
+    Array.prototype.sum = function (prop) {
+        var total = 0;
+        for ( var i = 0, _len = this.length; i < _len; i++ ) {
+            total += this[i][prop]
+        }
+        return total
+    };
