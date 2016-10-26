f5703e7eb02d050401374368749d246c967a432c
Oleksii Tatarikov <olta1014@netcracker.com>
2016-10-26 15:07:22 +0300

    fix invoice_service test

--- src/main/resources/static/resources/contragents/invoices_service_test.js
+++ src/main/resources/static/resources/contragents/invoices_service_test.js
@@ -1,16 +1,22 @@
-describe('sorting the list of users', function() {
+describe('invoicesService tests', function() {
+
+    beforeEach(module('inmarket.invoicesService'));
 
     var invoicesService;
     var $httpBackend;
     beforeEach(inject(function($injector) {
-        var $injector = angular.injector([ 'inmarket.invoicesService']);
         $httpBackend = $injector.get('$httpBackend');
         invoicesService = $injector.get('invoicesService');
     }));
 
+    it('checks that diff between same dates == 0', function() {
+        var currentDate = new Date();
+        expect(invoicesService.dateDiffInDays(currentDate, currentDate)).toEqual(0);
+    });
 
-
-    iit('sorts in descending order by default', function() {
-        expect(invoicesService.dateDiffInDays(new Date, new Date())).toEqual(0);
+    it('checks that diff between 30.04.2016 and 02.05.2016 == 3', function() {
+        var first = new Date(2016, 4, 30);
+        var second = new Date(2016, 5, 2);
+        expect(invoicesService.dateDiffInDays(first, second)).toEqual(3);
     });
 });
