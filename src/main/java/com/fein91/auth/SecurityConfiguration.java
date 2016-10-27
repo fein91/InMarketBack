1c367ab86026135373abab3a1705207eaba7c4a8
Oleksii Tatarikov <olta1014@netcracker.com>
2016-10-27 18:31:21 +0300

    security fixes

--- src/main/java/com/fein91/auth/SecurityConfiguration.java
+++ src/main/java/com/fein91/auth/SecurityConfiguration.java
@@ -9,10 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.csrf.*;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
@@ -55,23 +52,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .and()
                 .exceptionHandling()
                     .authenticationEntryPoint(restAuthenticationEntryPoint)
-                .and()
+                    .and()
                 .formLogin()
                     //.loginPage("/")
-                .loginProcessingUrl("/authenticate")
-                .successHandler(restAuthenticationSuccessHandler)
+                    .loginProcessingUrl("/authenticate")
+                    .successHandler(restAuthenticationSuccessHandler)
                     .failureHandler(restAuthenticationFailureHandler)
                     .permitAll()
-                .and()
+                    .and()
                 .logout()
-                .invalidateHttpSession(true)
+                    .invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID", "remember-me")
-                .permitAll()
-                .and()
+                    .permitAll()
+                    .and()
                 .csrf()
-                .csrfTokenRepository(csrfTokenRepository()).and()
-                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
-                    .httpBasic();
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .and()
+                //.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+                .httpBasic();
     }
 
     @Override
@@ -87,32 +85,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
     }
 
-    private Filter csrfHeaderFilter() {
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest request,
-                                            HttpServletResponse response, FilterChain filterChain)
-                    throws ServletException, IOException {
-                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
-                        .getName());
-                if (csrf != null) {
-                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-                    String token = csrf.getToken();
-                    if (cookie == null || token != null
-                            && !token.equals(cookie.getValue())) {
-                        cookie = new Cookie("XSRF-TOKEN", token);
-                        cookie.setPath("/");
-                        response.addCookie(cookie);
-                    }
-                }
-                filterChain.doFilter(request, response);
-            }
-        };
-    }
+//    private Filter csrfHeaderFilter() {
+//        return new OncePerRequestFilter() {
+//            @Override
+//            protected void doFilterInternal(HttpServletRequest request,
+//                                            HttpServletResponse response, FilterChain filterChain)
+//                    throws ServletException, IOException {
+//                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+//                        .getName());
+//                if (csrf != null) {
+//                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+//                    String token = csrf.getToken();
+//                    if (cookie == null || token != null
+//                            && !token.equals(cookie.getValue())) {
+//                        cookie = new Cookie("XSRF-TOKEN", token);
+//                        cookie.setPath("/");
+//                        response.addCookie(cookie);
+//                    }
+//                }
+//                filterChain.doFilter(request, response);
+//            }
+//        };
+//    }
 
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
-    }
+//    private CsrfTokenRepository csrfTokenRepository() {
+//        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+//        repository.setHeaderName("X-XSRF-TOKEN");
+//        return repository;
+//    }
 }
